fb72c9bf5d5b46e2c3cc76132cce0db685a9b36e
indigo-night <88883467+indigo-night@users.noreply.github.com>
2022-07-09 18:58:27 +0300

    Added pins

---
+++ Ana Kod/main/pins.ino
@@ -0,0 +1,26 @@
+
+//Line Following Sensors
+#define IR_SENSOR_RIGHT 9
+#define IR_SENSOR_LEFT 8
+#define IR_SENSOR_FAR_RIGHT 29
+#define IR_SENSOR_FAR_LEFT 28
+#define IR_SENSOR_MIDDLE 30
+
+#define MOTOR_SPEED 80
+
+//Right motor
+#define enableRightMotor 6
+#define rightMotorPin1 7
+#define rightMotorPin2 4
+
+//Left motor
+#define enableLeftMotor 5
+#define leftMotorPin1 2
+#define leftMotorPin2 3
+
+//Distance Sensor
+#define trigPin 10
+#define echoPin 11
+
+//Buzzer
+#define buzzer 22
