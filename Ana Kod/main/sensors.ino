a476ee6e71b84c90f21b20744b7adbabbd9cf2c4
indigo-night <88883467+indigo-night@users.noreply.github.com>
2022-07-12 14:41:28 +0300

    Initialized Sensors

---
+++ Ana Kod/main/sensors.ino
@@ -0,0 +1,57 @@
+long duration;
+int distance;
+
+initializeSensors() {
+  TCCR0B = TCCR0B & B11111000 | B00000010 ;
+  
+  //Motor
+  pinMode(enableRightMotor, OUTPUT);
+  pinMode(rightMotorPin1, OUTPUT);
+  pinMode(rightMotorPin2, OUTPUT);
+  
+  pinMode(enableLeftMotor, OUTPUT);
+  pinMode(leftMotorPin1, OUTPUT);
+  pinMode(leftMotorPin2, OUTPUT);
+  rotateMotor(0,0); 
+
+  //Line Following
+  pinMode(IR_SENSOR_RIGHT, INPUT);
+  pinMode(IR_SENSOR_LEFT, INPUT);
+  pinMode(IR_SENSOR_FAR_RIGHT, INPUT);
+  pinMode(IR_SENSOR_FAR_LEFT, INPUT);
+  pinMode(IR_SENSOR_MIDDLE, INPUT);
+    
+  //Distance Sensor
+  pinMode(trigPin, OUTPUT);
+  pinMode (echoPin, INPUT);
+  Serial.begin(9600);
+
+  //Buzzer
+  pinMode(buzzer, OUTPUT); 
+}
+
+
+static long currentSensorInfo[6];
+void GetSensorInfo()
+{
+    currentSensorInfo[0] = digitalRead(IR_SENSOR_FAR_LEFT);
+    currentSensorInfo[1] = digitalRead(IR_SENSOR_LEFT);
+    currentSensorInfo[2] = digitalRead(IR_SENSOR_MIDDLE);
+    currentSensorInfo[3] = digitalRead(IR_SENSOR_RIGHT);
+    currentSensorInfo[4] = digitalRead(IR_SENSOR_FAR_RIGHT);
+    
+    digitalWrite(trigPin, LOW);
+    delayMicroseconds(2);
+
+    digitalWrite(trigPin, HIGH);
+    delayMicroseconds(10);
+    digitalWrite(trigPin, LOW);
+
+    long duration = pulseIn(echoPin, HIGH);
+    int distance = duration*0.034/2;
+    
+    currentSensorInfo[5] = distance
+    
+    return;
+}
+
